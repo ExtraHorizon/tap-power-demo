014ce5f398349d98fdd206033bfd85e4cab4c31c
Filip Aben <filip.aben@extrahorizon.com>
2022-04-26 16:57:32 +0200

    chore: fix host

--- pwa/src/constants.ts
+++ pwa/src/constants.ts
@@ -1,5 +1,5 @@
 export const CLIENT_ID = '409ce9ba49c56cce31b9d2b1b2f5ed5ac01b4012';
-// export const HOST = 'https://api.dev.exh-sandbox.extrahorizon.io';
-export const HOST = 'https://e08b-84-192-54-31.ngrok.io';
+export const HOST = 'https://api.dev.exh-sandbox.extrahorizon.io';
+// export const HOST = 'https://e08b-84-192-54-31.ngrok.io';
 export const FIXED_PWD = 'ptL22AgLicG7';
 export const MAX_TICKS = 30;
